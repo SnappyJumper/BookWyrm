0f2e97e3b2d0e32a9bd4ea36694c517906113bf2
Oliver Griffin <olivergriffin2991@gmail.com>
2024-12-19 08:16:30 +0000

    preparing code for first deployment to Heroku

--- bookwyrm/urls.py
+++ bookwyrm/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from horde.views import test
+from django.urls import path, include
+from horde import views as horde_views
 
 urlpatterns = [
+    path('test/', horde_views.test, name='test'),
     path('admin/', admin.site.urls),
-    path('test/', test, name='test'),
 ]
