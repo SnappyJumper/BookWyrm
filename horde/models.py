b23ed944579d0d4b934097c2987e328d28db5ee7
Oliver Griffin <olivergriffin2991@gmail.com>
2024-12-21 16:24:06 +0000

    added Book and Author models to horde app

--- horde/models.py
+++ horde/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STAR = ((0, "1-5"),(1, "One Star"),(2, "Two Star"),(3, "Three Star"),(4, "Four Star"),(5, "Five Star"))
+STATUS = ((0, "Save as Draft"),(1, "Post"))
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    date_of_birth = models.DateField()
+    nationality = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    favourite_book = models.CharField(max_length=200)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    genre = models.CharField(max_length=50)
+    book_published = models.DateField()
+    review = models.TextField()
+    rating = models.IntegerField(choices=STAR, default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
+    review_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="horde_books")
+
+
