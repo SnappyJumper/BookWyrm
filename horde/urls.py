dd3a31a6540e24a9be0a3c51748f158bc76a6a28
Oliver Griffin <olivergriffin2991@gmail.com>
2024-12-29 13:01:26 +0000

    created views and url paths for author_bio and book_review and linked them to html templates of the same name in templates/horde. Added default images to the static folder and linked them to templates also.

--- horde/urls.py
+++ horde/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.HomeBookList.as_view(), name="home"),
     path("reviews/", views.BookList.as_view(), name="reviews"),
     path("authors/",views.AuthorList.as_view(), name ="authors"),
-    
+    path("<slug:slug>", views.book_review, name="book_review"),
+    path("<slug:slug>", views.author_bio, name="author_bio")
 ]
--- horde/views.py
+++ horde/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book, Author
 # Create your views here.
@@ -17,3 +17,47 @@ class HomeBookList(generic.ListView):
     queryset = Book.objects.all() 
     template_name = "horde/index.html"
     paginate_by = 6  
+
+def book_review(request, slug):
+    """
+    Displays an individual :model:'horde.Book'.
+
+    **Context**
+    ''book''
+        An instance of :model:'horde.Book'
+
+    **Template**
+
+    :template: 'horde/book_review.html'    
+    """
+
+    queryset = Book.objects.filter(status=1)
+    book = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "horde/book_review.html",
+        {"book": book},
+    )
+
+def author_bio(request, slug):
+    """
+    Displays an individual :model:'horde.Author'.
+
+    **Context**
+    ''author''
+        An instance of :model:'horde.Author'.
+    
+    **Template**
+
+    :template: 'horde/author_bio.html
+    """
+
+    queryset = Author.objects.filter(status=1)
+    author = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "horde/author_bio.html",
+        {"author": author},
+    )
