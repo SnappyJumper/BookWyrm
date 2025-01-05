33403bb34227bedbf0ccdf819349b0d2631e870a
Oliver Griffin <olivergriffin2991@gmail.com>
2025-01-05 15:32:23 +0000

    added additional JavaScript so that the slug_author field in the AuthorForm auto fills with the same text as the Name field as the user fills the name field

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const titleField = document.getElementById('id_title');
-    const slugField = document.getElementById('id_slug');
+    const titleField = document.getElementById("id_title");
+    const slugField = document.getElementById("id_slug");
+    const nameField = document.getElementById("id_name");
+    const slugAuthorField = document.getElementById("id_slug_author")
 
     if (titleField && slugField) {
-        titleField.addEventListener('input', function () {
+        titleField.addEventListener("input", function () {
             const slugValue = titleField.value
                 .toLowerCase()
                 .trim()
-                .replace(/[^a-z0-9\s-]/g, '') // Remove special characters
-                .replace(/\s+/g, '-') // Replace spaces with dashes
-                .replace(/-+/g, '-'); // Remove multiple consecutive dashes
+                .replace(/[^a-z0-9\s-]/g, "") // Remove special characters
+                .replace(/\s+/g, "-") // Replace spaces with dashes
+                .replace(/-+/g, "-"); // Remove multiple consecutive dashes
             slugField.value = slugValue;
         });
-    } 
+    }
+    
+    if (nameField && slugAuthorField) {
+        nameField.addEventListener("input", function () {
+            const slugAuthorValue = nameField.value
+                .toLowerCase()
+                .trim()
+                .replace(/[^a-z0-9\s-]/g, "") // Remove special characters
+                .replace(/\s+/g, "-") // Replace spaces with dashes
+                .replace(/-+/g, "-"); // Remove multiple consecutive dashes
+            slugAuthorField.value = slugAuthorValue;
+        });
+    }
 });
 
 function confirmDelete() {
